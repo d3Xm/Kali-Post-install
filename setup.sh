5dcb7d4fd5b96a323e58533743498155a225ed7f
d3Xm <76774296+d3Xm@users.noreply.github.com>
2021-05-18 17:22:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,13 @@ echo 'PS1="\[\e[31;1m\]\u\[\e[32;1m\]\[\e[37;2m\](\[\e[32;1m\]\w\[\e[37;1m\])\[\
 echo 'downloading tmux conf'
 wget https://raw.githubusercontent.com/d3Xm/Kali-Post-install/main/.tmux.conf
 
+
+printf '\n============================================================\n'
+printf '[+] Remove password requirement for sudo commands\n'
+printf '============================================================\n\n'
+sed 's/sudo/boop/g' /etc/sudoers >> /etc/sudoers
+echo '%sudo   ALL=(ALL:ALL) NOPASSWD' >> /etc/sudoers
+
 printf '\n============================================================\n'
 printf '[+] Enabling bash session logging\n'
 printf '============================================================\n\n'
@@ -258,11 +265,26 @@ sysctl -p
 neo4j start
 
 printf '\n============================================================\n'
-printf '[+] Installing Sublime Text\n'
+printf '[+] Installing Docker\n'
 printf '============================================================\n\n'
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
-apt-get -y install sublime-text
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | tee /etc/apt/sources.list.d/docker.list
+apt remove docker docker-engine docker.io
+apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl enable containerd
+curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+
+
+
+
+
+
+
+
 
 
