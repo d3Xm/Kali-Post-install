3d7187969d698de11e702ca8bd2024c0e25b113d
d3Xm <76774296+d3Xm@users.noreply.github.com>
2021-05-18 21:40:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,23 +59,23 @@ export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins"
 /usr/share/tmux-plugin-manager/scripts/install_plugins.sh
 mkdir -p "$HOME/Logs" 2>/dev/null
 
-grep -q 'TMUX_LOGGING' "/etc/profile" || echo '
-export HISTSIZE= 
-export HISTFILESIZE=
-export PROMPT_COMMAND="history -a"
-export HISTTIMEFORMAT="%F %T "
-setopt INC_APPEND_HISTORY 2>/dev/null
+#grep -q 'TMUX_LOGGING' "/etc/profile" || echo '
+#export HISTSIZE= 
+#export HISTFILESIZE=
+#export PROMPT_COMMAND="history -a"
+#export HISTTIMEFORMAT="%F %T "
+#setopt INC_APPEND_HISTORY 2>/dev/null
 
-logdir="$HOME/Logs"
-mkdir -p $logdir 2>/dev/null
-#gzip -q $logdir/*.log &>/dev/null
-export XDG_CONFIG_HOME="$HOME"
-export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins"
-if [[ ! -z "$TMUX" && -z "$TMUX_LOGGING" ]]; then
-    logfile="$logdir/tmux_$(date -u +%F_%H_%M_%S)_UTC.$$.log"
-    "$TMUX_PLUGIN_MANAGER_PATH/tmux-logging/scripts/start_logging.sh" "$logfile"
-    export TMUX_LOGGING="$logfile"
-fi' >> "/etc/profile"
+#logdir="$HOME/Logs"
+#mkdir -p $logdir 2>/dev/null
+##gzip -q $logdir/*.log &>/dev/null
+#export XDG_CONFIG_HOME="$HOME"
+#export TMUX_PLUGIN_MANAGER_PATH="$HOME/.tmux/plugins"
+#if [[ ! -z "$TMUX" && -z "$TMUX_LOGGING" ]]; then
+#    logfile="$logdir/tmux_$(date -u +%F_%H_%M_%S)_UTC.$$.log"
+#    "$TMUX_PLUGIN_MANAGER_PATH/tmux-logging/scripts/start_logging.sh" "$logfile"
+#    export TMUX_LOGGING="$logfile"
+#fi' >> "/etc/profile"
 
 normal_log_script='
 export HISTSIZE= 
