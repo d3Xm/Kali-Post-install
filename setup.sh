5f3d605ff3820d673199167e234ad0fdf197f77b
d3Xm <76774296+d3Xm@users.noreply.github.com>
2021-06-05 13:27:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -339,6 +339,7 @@ printf '     - exif\n'
 printf '============================================================\n\n'
 apt-get install -y gnome-screenshot libreoffice remmina gvfs-backends sshpass xclip grc exif libimage-exiftool-perl
 pip3 install flask-unsign
+export PATH=$PATH:/home/kali/.local/bin
 
 printf '\n============================================================\n'
 printf '[+] Increase open file limit\n'
